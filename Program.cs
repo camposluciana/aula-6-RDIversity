d1b7e3985d47a1462f72a2d5414466ac9eaab5f9
lucianacampos <lucianacristina.campos@gmail.com>
2026-01-12 22:34:36 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("----Verificando os lados de Polígonos ----");
+        Console.WriteLine("Digite o número de lados de um polígono regular: ");
+
+        int numeroDeLados = int.Parse(Console.ReadLine());
+
+        if (numeroDeLados == 3)
+        {
+            Console.WriteLine("O polígono é um triângulo!");
+        }
+        else if (numeroDeLados == 4)
+        {
+            Console.WriteLine("O polígono é um quadrado!");
+        }
+        else if (numeroDeLados == 5)
+        {
+            Console.WriteLine("O polígono é um pentágono!");
+        }
+        else
+        {
+            Console.WriteLine("Número de lados inválido.");
+        }
+    
+
